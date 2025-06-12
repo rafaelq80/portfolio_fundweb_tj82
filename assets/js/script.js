1834c4d6129fdca733032b599e77e69bbf781b61
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-06-12 18:40:21 -0300

    :lipstick: Revisão da Estilização da Página e da Responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 const sobre = document.querySelector('#about')
 
-const formulario = document.querySelector("#formulario")
+const formulario = document.querySelector('#formulario')
 
+// Expressão regular para validar o e-mail
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 async function getApiGithub() {
@@ -14,16 +15,18 @@ async function getApiGithub() {
 		// Converte a Resposta HTTP para o formato JSON
 		const perfil = await dadosPerfil.json()
 
-		// Criando o conteúdo da Seção about
+		// Cria o conteúdo da Seção about
 		let conteudo = `
-    
+
             <!-- Imagem da seção Sobre -->
             <img src="${perfil.avatar_url}" alt="Foto do perfil do Github - ${perfil.name}" />
 
             <!-- Texto da seção Sobre -->
             <article id="about_texto">
-                <h1>Sobre mim</h1>
-                <p>${perfil.bio}</p>
+                <h2>Sobre mim</h2>
+                <p>Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus quis, vehicula ac nisi. Quem manda
+                    na minha terra sou euzis! A ordem dos tratores não altera o pão duris. Copo furadis é disculpa de
+                    bebadis, arcu quam euismod magna.</p>
 
                 <div id="about_github" class="flex sobre_github">
                     <a href="${perfil.html_url}" target="_blank" class="botao">Github</a>
@@ -33,53 +36,60 @@ async function getApiGithub() {
             </article>
             
     `
-		// Adicionar o conteúdo na página index.html, na Seção about
+		// Adiciona o conteúdo na página index.html, na Seção about
 		sobre.innerHTML += conteudo
-
 	} catch (error) {
 		console.error(error)
 	}
 }
 
-formulario.addEventListener("submit", function(event){
-
+formulario.addEventListener('submit', function (event) {
+	
+    // Impede o envio do formulário
     event.preventDefault()
 
-    const campoNome = document.querySelector("#nome")
-    const txtNome = document.querySelector("#txtNome")
+    // Seleciona o campo de nome
+	const campoNome = document.querySelector('#nome')
+	const txtNome = document.querySelector('#txtNome')
 
-    if(campoNome.value.length < 3){
-        txtNome.innerHTML = "O Nome deve ter no mínimo 3 caracteres"
-        campoNome.focus()
-        return
-    }else{
-        txtNome.innerHTML = ""
-    }
+    // Verifica se o campo de nome tem no mínimo 3 caracteres
+	if (campoNome.value.length < 3) {
+		txtNome.innerHTML = 'O Nome deve ter no mínimo 3 caracteres'
+		campoNome.focus()
+		return
+	} else {
+		txtNome.innerHTML = ''
+	}
 
-     const campoEmail = document.querySelector("#email")
-    const txtEmail = document.querySelector("#txtEmail")
+    // Seleciona o campo de e-mail
+	const campoEmail = document.querySelector('#email')
+	const txtEmail = document.querySelector('#txtEmail')
 
-    if(!campoEmail.value.match(emailRegex)){
-        txtEmail.innerHTML = "Digite um e-mail válido"
-        campoEmail.focus()
-        return
-    }else{
-        txtEmail.innerHTML = ""
-    }
+    // Verifica se o campo de e-mail é válido
+	if (!campoEmail.value.match(emailRegex)) {
+		txtEmail.innerHTML = 'Digite um e-mail válido'
+		campoEmail.focus()
+		return
+	} else {
+		txtEmail.innerHTML = ''
+	}
 
-    const campoAssunto = document.querySelector("#assunto")
-    const txtAssunto = document.querySelector("#txtAssunto")
+    // Seleciona o campo de assunto
+	const campoAssunto = document.querySelector('#assunto')
+	const txtAssunto = document.querySelector('#txtAssunto')
 
-    if(campoAssunto.value.length < 5){
-        txtAssunto.innerHTML = "O Assunto deve ter no mínimo 5 caracteres"
-        campoAssunto.focus()
-        return
-    }else{
-        txtAssunto.innerHTML = ""
-    }
+    // Verifica se o campo de assunto tem no mínimo 5 caracteres
+	if (campoAssunto.value.length < 5) {
+		txtAssunto.innerHTML = 'O Assunto deve ter no mínimo 5 caracteres'
+		campoAssunto.focus()
+		return
+	} else {
+		txtAssunto.innerHTML = ''
+	}
 
-    // Enviar o e-mail
-    formulario.submit()
+    // Envia o e-mail
+	formulario.submit()
 })
 
+// Chama a função para buscar os dados do perfil do Github
 getApiGithub()
